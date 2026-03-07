7285d7824d59de09f3201862f9e564e58eb99ce6
ziropu <datutarousumile@gmail.com>
2026-03-07 15:38:28 +0900

    ヘルプ書き込み

--- FemBoy/Program.cs
+++ FemBoy/Program.cs
@@ -3,7 +3,21 @@ class FemBoy
 {
     static void Main(String[] args)
     {
-        // See https://aka.ms/new-console-template for more information
-        Console.WriteLine("I'm FEMBOY");
+        if (args.Contains("--help"))
+        {
+            Console.WriteLine("使い方" + Environment.NewLine +
+                "1.Picフォルダに画像を入れる。" + Environment.NewLine +
+                "2.Musicフォルダに使いたい音楽を入れる" + Environment.NewLine +
+                "3.format.txtにアーティスト情報と曲名とBPMを入れる" + Environment.NewLine +
+                "4.準備が完了したら femboy --comeshotで実行する");
+        }
+        else if (args.Contains("--comeshot"))
+        {
+            Console.WriteLine("ComeComeCome...ComeShot!!!");
+        }
+        else
+        {
+            Console.WriteLine("Do it --help!");
+        }
     }
 }
